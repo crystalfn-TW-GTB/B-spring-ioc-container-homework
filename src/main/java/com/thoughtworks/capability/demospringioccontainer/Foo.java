10034d7f453d8ffb804fd7d6a2ac218c085e5d7a
crystalfn <969274162@qq.com>
2020-11-09 00:10:11 +0800

    [fangni] feat: solve problem3 with @PostConstruct

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/Foo.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/Foo.java
@@ -3,11 +3,18 @@ package com.thoughtworks.capability.demospringioccontainer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+
 @Component
 public class Foo {
 
     @Autowired
     private Bar bar;
+
+    @PostConstruct
+    public void init() {
+        bar.setFoo(this);
+    }
 
     public void hi() {
         System.out.println("Hi, " + bar.name());
